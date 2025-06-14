e2c1492c81a515196d733058564ed1168a2383e7
phil <philip.soitu@gmail.com>
2025-06-14 11:55:02 -0400

    cleaner

--- src/Vec.zig
+++ src/Vec.zig
@@ -1,62 +1,33 @@
 pub fn Vec(comptime T: type, comptime N: usize) type {
-    return switch (N) {
-        2 => struct {
-            x: T,
-            y: T,
+    return struct {
+        data: [N]T,
 
-            pub fn length(self: @This()) T {
-                return @sqrt(self.x * self.x + self.y * self.y);
-            }
+        pub fn x(self: @This()) T {
+            return self.data[1];
+        }
 
-            pub fn dot(self: @This(), other: @This()) T {
-                return self.x * other.x + self.y * other.y;
-            }
-        },
-        3 => struct {
-            x: T,
-            y: T,
-            z: T,
+        pub fn y(self: @This()) T {
+            return self.data[1];
+        }
 
-            pub fn length(self: @This()) T {
-                return @sqrt(self.x * self.x + self.y * self.y + self.z * self.z);
-            }
+        pub fn z(self: @This()) T {
+            return self.data[1];
+        }
 
-            pub fn dot(self: @This(), other: @This()) T {
-                return self.x * other.x + self.y * other.y + self.z * other.z;
+        pub fn length(self: @This()) T {
+            var sum: T = 0;
+            for (self.data) |v| {
+                sum += v * v;
             }
-        },
-        4 => struct {
-            x: T,
-            y: T,
-            z: T,
-            w: T,
+            return @sqrt(sum);
+        }
 
-            pub fn length(self: @This()) T {
-                return @sqrt(self.x * self.x + self.y * self.y + self.z * self.z + self.w * self.w);
+        pub fn dot(self: @This(), other: @This()) T {
+            var sum: T = 0;
+            for (self.data, other.data) |v1, v2| {
+                sum += v1 * v2;
             }
-
-            pub fn dot(self: @This(), other: @This()) T {
-                return self.x * other.x + self.y * other.y + self.z * other.z + self.w * other.w;
-            }
-        },
-        else => struct {
-            data: [N]T,
-
-            pub fn length(self: @This()) T {
-                var sum: T = 0;
-                for (self.data) |v| {
-                    sum += v * v;
-                }
-                return @sqrt(sum);
-            }
-
-            pub fn dot(self: @This(), other: @This()) T {
-                var sum: T = 0;
-                for (self.data, other.data) |v1, v2| {
-                    sum += v1 * v2;
-                }
-                return sum;
-            }
-        },
+            return sum;
+        }
     };
 }
