d29d418ff9d6d20bab5542a06b95bc3dde56bf20
phil <philip.soitu@gmail.com>
2025-06-15 23:28:32 -0400

    that's not what a triangle looks like...

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const ppm = @import("ppm.zig");
+const math = @import("math.zig");
 
 pub const Pixel = struct {
     r: u8,
@@ -14,21 +15,45 @@ pub fn main() !void {
 
     var framebuffer: [height][width]Pixel = std.mem.zeroes([height][width]Pixel);
 
+    const a = math.Vec2f{
+        .x = 0.2 * width,
+        .y = 100,
+    };
+    const b = math.Vec2f{
+        .x = 100,
+        .y = 200,
+    };
+    const c = math.Vec2f{
+        .x = 200,
+        .y = 200,
+    };
+
     for (0..height) |y| {
         for (0..width) |x| {
             const r_ratio: f64 = @as(f64, @floatFromInt(x)) / @as(f64, @floatFromInt(width - 1));
             const g_ratio: f64 = @as(f64, @floatFromInt(y)) / @as(f64, @floatFromInt(height - 1));
             const b_ratio: f64 = 0.0;
 
-            const r: u8 = @intFromFloat(255.99 * r_ratio);
-            const g: u8 = @intFromFloat(255.99 * g_ratio);
-            const b: u8 = @intFromFloat(255.99 * b_ratio);
+            const red: u8 = @intFromFloat(255.99 * r_ratio);
+            const green: u8 = @intFromFloat(255.99 * g_ratio);
+            const blue: u8 = @intFromFloat(255.99 * b_ratio);
 
-            const pixel: Pixel = .{
-                .r = r,
-                .g = g,
-                .b = b,
+            var pixel: Pixel = .{
+                .r = red,
+                .g = green,
+                .b = blue,
             };
+
+            if (math.pointInTriangle(a, b, c, math.Vec2f{
+                .x = @floatFromInt(x),
+                .y = @floatFromInt(y),
+            })) {
+                pixel = .{
+                    .r = 0,
+                    .g = 0,
+                    .b = 255,
+                };
+            }
 
             framebuffer[y][x] = pixel;
         }
---
+++ src/math.zig
@@ -0,0 +1,40 @@
+const std = @import("std");
+
+pub const Vec2f = struct {
+    x: f64,
+    y: f64,
+};
+
+// that thing we learned in linear algebra
+pub fn dotProduct(a: Vec2f, b: Vec2f) f64 {
+    return a.x * b.x + a.y * b.y;
+}
+
+/// calculates vector 90 degrees clockwise
+pub fn perpendicular(vec: Vec2f) Vec2f {
+    return Vec2f{
+        .x = vec.y,
+        .y = -vec.x,
+    };
+}
+
+/// checks if point p is on the right side of a -> b
+pub fn pointOnRightSide(a: Vec2f, b: Vec2f, p: Vec2f) bool {
+    const ap = Vec2f{
+        .x = p.x - a.x,
+        .y = p.y - p.y,
+    };
+    const ab_perp = perpendicular(Vec2f{
+        .x = b.x - a.x,
+        .y = b.y - a.y,
+    });
+    return dotProduct(ap, ab_perp) >= 0;
+}
+
+// checks if point p is in triangle abc
+pub fn pointInTriangle(a: Vec2f, b: Vec2f, c: Vec2f, p: Vec2f) bool {
+    const side_ab = pointOnRightSide(a, b, p);
+    const side_bc = pointOnRightSide(b, c, p);
+    const side_ca = pointOnRightSide(c, a, p);
+    return side_ab == side_bc and side_bc == side_ca;
+}
